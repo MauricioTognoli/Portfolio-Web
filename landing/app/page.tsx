adbee94d2c32bded870410c67cf059e6a456d035
MauricioTognoli <tognolimauricio@gmail.com>
2025-02-11 20:02:37 -0300

    transition para pages

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -1,8 +1,10 @@
 import Cover from "@/components/Cover";
+import TransitionPage from "@/components/Transition-page";
 
 export default function Home() {
   return (
     <main>
+      <TransitionPage/>
       <div className="flex min-h-[100vh] h-full bg-no-repeat bg-gardient-cover">
         <Cover/>
         <p>Home</p>
