76d3428f27567ac708bbdbc334f9f3ce688716b5
MauricioTognoli <tognolimauricio@gmail.com>
2025-02-19 15:36:18 -0300

    fix responsive en portfolio

--- landing/components/Portfolio-box.tsx
+++ landing/components/Portfolio-box.tsx
@@ -18,13 +18,13 @@ const PortfolioBox = (props: PortfolioBoxProps) => {
     return (
         <div
             key={id}
-            className="p-4 border border-teal-50 rounded-xl"
+            className="p-4 border border-teal-50 shadow-2xl shadow-secondary rounded-xl"
         >
             <h3 className="mb-4 text-xl">{title}</h3>
             <Image
                 src={image}
                 alt="Image"
-                width={200} height={200} className="w-full md:w-[200px] rounded-2xl h-auto"
+                width={200} height={200} className="w-full md:w-[200px] md:h-[110px] rounded-2xl h-auto"
             />
 
             <div className="flex gap-5 mt-5">
--- landing/data.tsx
+++ landing/data.tsx
@@ -144,60 +144,18 @@ export const serviceData = [
 export const dataPortfolio = [
     {
         id: 1,
-        title: "Web Pro",
-        image: "/image-1.jpg",
-        urlGithub: "#!",
-        urlDemo: "#!",
+        title: "NexoTv",
+        image: "/nexo.jpg",
+        urlGithub: "https://github.com/MauricioTognoli/NexoTv.git",
+        urlDemo: "https://nexo-tv.vercel.app/",
     },
     {
         id: 2,
-        title: "Desarrollo Web Ágil",
-        image: "/image-2.jpg",
-        urlGithub: "#!",
-        urlDemo: "#!",
+        title: "WeatherWeb",
+        image: "/weather-web.png",
+        urlGithub: "https://github.com/MauricioTognoli/WeatherWeb.git",
+        urlDemo: "https://weather-web-six-omega.vercel.app/",
     },
-    {
-        id: 3,
-        title: "Estrategias Web",
-        image: "/image-3.jpg",
-        urlGithub: "#!",
-        urlDemo: "#!",
-    },
-    {
-        id: 4,
-        title: "Ideas Creativas",
-        image: "/image-4.jpg",
-        urlGithub: "#!",
-        urlDemo: "#!",
-    },
-    {
-        id: 5,
-        title: "Webs Impactantes",
-        image: "/image-5.jpg",
-        urlGithub: "#!",
-        urlDemo: "#!",
-    },
-    {
-        id: 6,
-        title: "Web Dinámica",
-        image: "/image-6.jpg",
-        urlGithub: "#!",
-        urlDemo: "#!",
-    },
-    {
-        id: 7,
-        title: "Dark Web ",
-        image: "/image-7.jpg",
-        urlGithub: "#!",
-        urlDemo: "#!",
-    },
-    {
-        id: 8,
-        title: "E-commerce web",
-        image: "/image-8.jpg",
-        urlGithub: "#!",
-        urlDemo: "#!",
-    }
 ];
 
 export const dataTestimonials = [
